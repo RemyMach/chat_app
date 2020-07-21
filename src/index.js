1919870ac8c4469ff6bc5dde5f44024b2b0fef24
RemyMach <remygt@hotmail.fr>
2020-07-21 10:36:22 +0200

    fonction de connection entre le socket et le serveur, client représenté par chat.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const socketio = require('socket.io')
 const app = express()
 // quand on ne le fait pas, c'est quelque chose qui est fait en back
 const server = http.createServer(app)
+// on le crée comme cela pour pouvoir le passer au serveur
+const io = socketio(server)
 
 app.use(express.json())
 const port = process.env.PORT || 3000
@@ -13,6 +15,10 @@ const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public/')
 app.use('/',express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log('connection')
+})
 
 app.get('/', (req, res) => {
     res.render('html/index.html')
